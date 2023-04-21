a2842165cbdaff5b4702662d7bbac7c6c56dbe38
Menn <m@seedwebs.com>
2023-04-21 08:15:03 +0700

    add pattern

---
+++ patterns/cover.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Title: Cover
+ * Slug: pai/cover
+ * Category: Pai
+ */
+
+?>
+
+
+<!-- wp:cover {"url":"<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/banner.jpg","id":82,"dimRatio":0,"minHeight":600,"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"},"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-cover" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40);min-height:600px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-82" alt="" src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/assets/images/banner.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"50px"},"spacing":{"margin":{"bottom":"var:preset|spacing|20"}}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="margin-bottom:var(--wp--preset--spacing--20);font-size:50px">P A I</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">One Page Block Theme for Resort & Homestay by Seed Webs</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+
---
+++ patterns/welcome.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: Welcome
+ * Slug: pai/welcome
+ * Category: Pai
+ */
+
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div id="welcome" class="wp-block-group"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">WELCOME TO PAI</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Pai in Thai language means bamboo (ไผ่) or the name of village in northern Thailand (ปาย) that our Seed Webs company is located. We create this block theme (Full Site Editing WordPress theme) for resort & homestay businesses. This theme is free and open source, distributed under the terms of the GNU General Public License as published by the Free Software Foundation.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
